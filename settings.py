7084076bd61d631b55d43cd9e72441c2744266bc
Tajinder <tajinder.chahal14@gmail.com>
2020-01-20 15:30:56 -0800

    db path changed

--- settings.py
+++ settings.py
@@ -87,7 +87,7 @@ WSGI_APPLICATION = 'wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR + '/family_tree', 'db.sqlite3'),
+        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
     }
 }
 
